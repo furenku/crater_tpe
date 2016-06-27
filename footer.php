83892cbae54410ad13678d0a85da538acb91c26a
josecaos <jazzvoon@gmail.com>
2016-06-27 13:23:17 -0500

    footer + fontawesome

--- footer.php
+++ footer.php
@@ -3,8 +3,23 @@
 </div><!-- #menu-y-principal -->
 
 <!-- footer#pie_pagina.row.expanded.h_5vh -->
-<footer id="pie_pagina" class="row expanded h_5vh">
-   Pie de página
+<footer id="pie_pagina" class="row expanded h_10vh">
+
+  <div id="pie_pagina_copyright" class="small-6 medium-5 large-4 columns">
+
+    <p class="small-12 text-center vcenter fontM bold"> <i class="fa fa-copyright fontS"></i> Cooperativa | <?php echo date("Y"); ?></p>
+
+  </div>
+
+  <div id="pie_pagina_redes" class="small-6 medium-7 large-8 columns">
+<ul class="small-12 columns m0">
+  <li class="small-3 columns h_100 p0 m0 fontM"><i class="small-12 fa fa-github vcenter"></i></li>
+  <li class="small-3 columns h_100 p0 m0 fontM"><i class="small-12 fa fa-twitter vcenter"></i></li>
+  <li class="small-3 columns h_100 p0 m0 fontM"><i class="small-12 fa fa-facebook vcenter"></i></li>
+  <li class="small-3 columns h_100 p0 m0 fontM"><i class="small-12 fa fa-instagram vcenter"></i></li>
+</ul>
+  </div>
+
 </footer>
 
 
