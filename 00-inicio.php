0f0fdb427538d4011f26906310733884e6bd150b
josecaos <jazzvoon@gmail.com>
2016-07-12 19:57:04 -0500

    WF home seccion ultimos productos del catalogo

--- 00-inicio.php
+++ 00-inicio.php
@@ -5,6 +5,7 @@ Template Name: Inicio
 
 get_header();
 
+get_template_part("secciones/02-inicio/01-catalogo");
 
 
 get_footer();
---
+++ secciones/00-compartidas/01-texto-descriptivo-seccion.php
@@ -0,0 +1,17 @@
+<div id="texto-descriptivo-contenedor" class="small-12 columns p2 h_20 h_sm_25 h_md_20">
+
+  <div id="texto-descriptivo-texto" class="columns p0 h_70 text-center">
+
+    <h2 class="small-12 vcenter fontL font_md_M font_sm_S">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore cum sunt doloremque mollitia quos temporibus sint autem corporis iure veritatis praesentium ut, non. Ratione, natus modi!
+  </h2>
+
+  </div>
+  <div id="texto-descriptivo-boton" class="columns p0 h_30 align-center">
+
+    <a href="#" class="small-12 h_100 fontM text-center">Ir a sección</a>
+
+  </div>
+
+
+</div>
