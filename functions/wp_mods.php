ee196e37d7fd14d6dd1d33a7669e15c869e002f8
furenku <furenku@gmail.com>
2016-07-14 13:50:20 -0500

    clases colores. portada catalogo aun sin scroll

---
+++ functions/wp_mods.php
@@ -0,0 +1,22 @@
+<?php
+
+add_filter('show_admin_bar', '__return_false');
+
+
+add_action( 'init', 'add_excerpts_to_pages' );
+function add_excerpts_to_pages() {
+   add_post_type_support( 'product', 'excerpt' );
+   // add_post_type_support( 'page', 'excerpt' );
+
+}
+
+
+function registrar_menus() {
+
+   register_nav_menu('menu-principal',__( 'Menú Principal' ));
+
+}
+add_action( 'init', 'registrar_menus' );
+
+
+add_theme_support('post-thumbnails');
--- secciones/05-catalogo/00-portada.php
+++ secciones/05-catalogo/00-portada.php
@@ -1,1 +1,34 @@
-<h1>Catálogo</h1>
+<?php
+
+// Catálogo
+
+$paginas_catalogo = get_pages( array( 'child_of' => get_page_by_title("Catálogo")->ID ) );
+
+?>
+
+<h1 class="p5">Catálogo</h1>
+
+<div id="catalogo-enlaces" class="mt2 p5">
+
+   <?php foreach( $paginas_catalogo as $pagina ) :
+      $ID = $pagina -> ID;
+      $titulo = apply_filters('the_title', $pagina -> post_title );
+      $texto = apply_filters('the_excerpt', $pagina -> post_content );
+      $link = get_the_permalink( $ID );
+   ?>
+   <div class="columns medium-4 h_a p5 text-center" data-scrollTo="<?php echo $ID; ?>">
+
+      <div class="button p5 color_blanco_bg color_primario_hover_bg color_negro color_blanco_hover">
+         <div class="imagen h_25vh imgLiquid imgLiquidFill">
+            <?php echo get_the_post_thumbnail( $ID ); ?>
+         </div>
+         <h3 class="p5"><?php echo $titulo; ?></h3>
+         <p class="extracto fontM"><?php echo $texto; ?></p>
+      </div>
+
+   </div>
+
+
+   <?php endforeach; ?>
+
+</div>
