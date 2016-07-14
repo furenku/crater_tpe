b4bfbf06208dbc3763466b0e39d905585904ac45
furenku <furenku@gmail.com>
2016-07-14 15:06:24 -0500

    catalogo - colecciones - jalar cats woocommerce

--- secciones/05-catalogo/01-colecciones.php
+++ secciones/05-catalogo/01-colecciones.php
@@ -2,47 +2,46 @@
    <?php
 
    $taxonomy     = 'product_cat';
-   $orderby      = 'name';
-   $show_count   = 0;      // 1 for yes, 0 for no
-   $pad_counts   = 0;      // 1 for yes, 0 for no
-   $hierarchical = 1;      // 1 for yes, 0 for no
+   $parent       = get_term_by( 'name', 'Colecciones', 'product_cat' )->term_id;
+   $orderby      = 'ASC';
+   $show_count   = 1;
+   $pad_counts   = 0;
+   $hierarchical = 1;
    $title        = '';
-   $empty        = 0;
+   $empty        = 1;
 
    $args = array(
       'taxonomy'     => $taxonomy,
+      'parent'       => $parent,
+      'child_of'     => $parent,
       'orderby'      => $orderby,
       'show_count'   => $show_count,
       'pad_counts'   => $pad_counts,
       'hierarchical' => $hierarchical,
       'title_li'     => $title,
-      'hide_empty'   => $empty
+      'hide_empty'   => $empty,
    );
-   $all_categories = get_categories( $args );
-   foreach ($all_categories as $cat) {
-      if($cat->category_parent == 0) {
-         $category_id = $cat->term_id;
-         echo '<br /><a href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
 
-         $args2 = array(
-            'taxonomy'     => $taxonomy,
-            'child_of'     => 0,
-            'parent'       => $category_id,
-            'orderby'      => $orderby,
-            'show_count'   => $show_count,
-            'pad_counts'   => $pad_counts,
-            'hierarchical' => $hierarchical,
-            'title_li'     => $title,
-            'hide_empty'   => $empty
-         );
-         $sub_cats = get_categories( $args2 );
-         if($sub_cats) {
-            foreach($sub_cats as $sub_category) {
-               echo  $sub_category->name ;
-            }
-         }
-      }
-   }
+   $all_categories = get_categories( $args );
+   // var_dump( $all_categories );
+   foreach ($all_categories as $cat) {
+      $category_id = $cat->term_id;
+      ?>
+
+      <a href="<?php echo get_term_link($cat->slug, 'product_cat'); ?>" class="columns medium-6">
+         <?php
+         $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
+         $image = wp_get_attachment_url( $thumbnail_id );
+         ?>
+         <div class="imagen h_25vh imgLiquid imgLiquidFill">
+            <img src="<?php echo $image; ?>" alt="">
+         </div>
+         <?php echo $cat->name; ?>
+      </a>
+
+      <?php
+
+}
 
    ?>
 </div>
